57b87af43166cc013f56ef557147ee1601f76b1e
Wilco Boode <boode.w@buas.nl>
2021-07-16 14:07:37 +0200

    Finished the first working version of the getInOutFromBeacons. A manual check is required, but this makes it a lot easier already. A video is in the markiezenhof project but will be documented later

--- matlabtools/PositioningTools/getinoutfrombeacons.m
+++ matlabtools/PositioningTools/getinoutfrombeacons.m
@@ -18,21 +18,25 @@ function out = getinoutfrombeacons(cfg)
 %   cfg.checkdata = true; %Do you wish to show and evaluate the data before accepting the in/out moments?
 %   cfg.minstrength = 80; %What is the minimal strength required to be a valid start/endpoint. Lower = stronger signal required (as its signal delay)
 %
-%Wilco, 21/06/2021
+%Wilco, 16/76/2021
+%
+%WARNING
+%Function is not yet extensively tested, and misses some documentation. Please consider this while
+%working with the data.
 
 %% Checks for cfg Values
-if (~isfield(cfg,'inbeacon'))
-    if (isfield(cfg,'outbeacon'))
-        warning('no in beacon indicated, assuming in and out beacon are the same');
-        cfg.inbeacon = cfg.outbeacon;
+if (~isfield(cfg,'inbeacons'))
+    if (isfield(cfg,'outbeacons'))
+        warning('no in beacons indicated, assuming in and out beacons are the same but flipped');
+        cfg.inbeacons = flip(cfg.outbeacons);
     else
         error('no in/out beacons indicated')
     end
 end
-if (~isfield(cfg,'outbeacon'))
-    if (isfield(cfg,'inbeacon'))
-        warning('no out beacon indicated, assuming in and out beacon are the same');
-        cfg.outbeacon = cfg.inbeacon;
+if (~isfield(cfg,'outbeacons'))
+    if (isfield(cfg,'inbeacons'))
+        warning('no out beacon indicated, assuming in and out beacon are the same but reversed');
+        cfg.outbeacons = flip(cfg.inbeacons);
     else
         error('no in/out beacons indicated')
     end
@@ -77,75 +81,82 @@ else
     beaconData = cfg.beacondata;
 end
 
-%Inex and get data from in/out beacon (OLD)
-inbeaconIndex = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.inbeacon));
-outbeaconIndex = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.outbeacon));
+%Index and get data from in/out beacon (OLD)
+%inbeaconIndex = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.inbeacon));
+%outbeaconIndex = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.outbeacon));
 
-indata = beaconData.beaconvalues.(inbeaconIndex);
-outdata = beaconData.beaconvalues.(outbeaconIndex);
+%indata = beaconData.beaconvalues.(inbeaconIndex);
+%outdata = beaconData.beaconvalues.(outbeaconIndex);
 
 
 %Index and get data from beacons
 beaconMap = containers.Map();
 
+%Get the indexes of the incoming beacons
 inbeaconsIndex = [];
 for i =1 : length(cfg.inbeacons)
     inbeaconsIndex = [inbeaconsIndex ; beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.inbeacons(i)))];
-    beaconMap(string(cfg.inbeacons(i))) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.inbeacons(i)))
+    beaconMap(string(cfg.inbeacons(i))) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.inbeacons(i)));
 end
 
+%Get the indexes of the outgoing beacons
 outbeaconsIndex = [];
 for i =1 : length(cfg.inbeacons)
     outbeaconsIndex = [outbeaconsIndex ; beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.outbeacons(i)))];
-    beaconMap(string(cfg.outbeacons(i))) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.outbeacons(i)))
+    beaconMap(string(cfg.outbeacons(i))) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.outbeacons(i)));
 end
 
-allbeaconsIndex = unique(vertcat(inbeaconsIndex,outbeaconsIndex))
+%allbeaconsIndex = unique(vertcat(inbeaconsIndex,outbeaconsIndex));
 
+%get the index of the middle beacons
 midbeaconIndex = [];
 midbeaconIndex = [midbeaconIndex ; beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.middlebeacon))];
-beaconMap(string(cfg.middlebeacon)) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.middlebeacon))
+beaconMap(string(cfg.middlebeacon)) = beaconData.beaconnames(find(beaconData.beaconMeta.('BeaconID') == cfg.middlebeacon));
 
-allbeaconsIndex = unique(vertcat(allbeaconsIndex,midbeaconIndex))
+%allbeaconsIndex = unique(vertcat(allbeaconsIndex,midbeaconIndex));
 
+%Get the signal data from all indicated beacons
 for k = keys(beaconMap)
-    k
-    beaconMap(k{1})
-    allbeaconsData.(beaconMap(k{1})) = beaconData.beaconvalues.(beaconMap(k{1}))
+    allbeaconsData.(beaconMap(k{1})) = beaconData.beaconvalues.(beaconMap(k{1}));
 end
 
 %% InOut Detection
 repeatdetection = 'y';
 while (repeatdetection == 'y')
     
-    %% Single In/Out beacon detection
-    
-    innandata = indata;
-    innandata(innandata > cfg.minstrength) = NaN;
-    [peaks,peakloc] = findpeaks(-innandata,'MinPeakProminence',cfg.prominence);
-    if height(peakloc) == 0
-        warning('No Input Peaks Found, using first timepoint');
-        inpeak = beaconData.time(1)+1;
-        cfg.checkdata = true;
-    else
-        inpeak = peakloc(1);
+    if exist('fig')
+        if ishandle(fig)
+            close(fig)
+        end
     end
     
-    outnnandata = outdata;
-    outnnandata(outnnandata > cfg.minstrength) = NaN;
-    [peaks,peakloc] = findpeaks(-outnnandata,'MinPeakProminence',cfg.prominence);
-    if height(peakloc) == 0
-        warning('No Output Peaks Found, using last time point');
-        outpeak = beaconData.time(height(beaconData.time))+1;
-        cfg.checkdata = true;
-    else
-        outpeak = peakloc(height(peakloc));
-    end
-    
-    if (outpeak < inpeak)
-        warning('Out Time is before In Time');
-        cfg.checkdata = true;
-    end
+    %% Single In/Out beacon detection Will be disabled when multi-detection works
+%     innandata = indata;
+%     innandata(innandata > cfg.minstrength) = NaN;
+%     [peaks,peakloc] = findpeaks(-innandata,'MinPeakProminence',cfg.prominence);
+%     if height(peakloc) == 0
+%         warning('No Input Peaks Found, using first timepoint');
+%         inpeak = beaconData.time(1)+1;
+%         cfg.checkdata = true;
+%     else
+%         inpeak = peakloc(1);
+%     end
+%     
+%     outnnandata = outdata;
+%     outnnandata(outnnandata > cfg.minstrength) = NaN;
+%     [peaks,peakloc] = findpeaks(-outnnandata,'MinPeakProminence',cfg.prominence);
+%     if height(peakloc) == 0
+%         warning('No Output Peaks Found, using last time point');
+%         outpeak = beaconData.time(height(beaconData.time))+1;
+%         cfg.checkdata = true;
+%     else
+%         outpeak = peakloc(height(peakloc));
+%     end
+%     
+%     if (outpeak < inpeak)
+%         warning('Out Time is before In Time');
+%         cfg.checkdata = true;
+%     end
     
     
     %% NEW SECTION / Multi beacon Detection
@@ -166,24 +177,95 @@ while (repeatdetection == 'y')
     %2. combine, putting the peaks for all in/out beacons after another
     %3.
     
-    %allbeaconsData
+    beaconsData = allbeaconsData;
+    fields = fieldnames (beaconsData);
+    allpeaks = NaN(1,length(beaconsData.(string(fields(1)))));
+    allinpeaks = NaN(1,length(beaconsData.(string(fields(1)))));
+    alloutpeaks = NaN(1,length(beaconsData.(string(fields(1)))));
     
-    fields = fieldnames (allbeaconsData);
-    allpeaks = NaN(1,length(allbeaconsData.(string(fields(1)))));
     for k = keys(beaconMap)
-        allbeaconsData.(beaconMap(k{1}))(allbeaconsData.(beaconMap(k{1})) > cfg.minstrength) = NaN;
-        [peaks,peakloc] = findpeaks(-allbeaconsData.(beaconMap(k{1})),'MinPeakProminence',cfg.prominence);
-        allpeaks(peakloc) = cellfun(@str2num,k);
+        beaconsData.(beaconMap(k{1}))(beaconsData.(beaconMap(k{1})) > cfg.minstrength) = NaN;
+        [peaks,peakloc] = findpeaks(-beaconsData.(beaconMap(k{1})),'MinPeakProminence',cfg.prominence);
+        allpeaks(peakloc) = cellfun(@str2num,k);        
+        if max(ismember(cfg.inbeacons,cellfun(@str2num,k))) == 1
+            allinpeaks(peakloc) = cellfun(@str2num,k);            
+        end        
+        if max(ismember(cfg.outbeacons,cellfun(@str2num,k))) == 1
+            alloutpeaks(peakloc) = cellfun(@str2num,k);            
+        end       
     end
     
-    allpeaksnonan = allpeaks
-    allpeaksnonan(isnan(allpeaksnonan)) = [];
-    %allbeaconsData
+    %allpeaksnonan = allpeaks;
+    %allpeaksnonan(isnan(allpeaksnonan)) = [];
     
-    %GetInPeak
+    %GetInPeaks
+    inpeaks = [];
+    for i = 1:length(allinpeaks)
+        if allinpeaks(i) == cfg.inbeacons(1)
+            comingpeaks = allinpeaks(i:length(allinpeaks));
+            comingpeaks(isnan(comingpeaks)) = [];
+            if length(comingpeaks) - length(cfg.inbeacons) >= 0
+                if comingpeaks(1:length(cfg.inbeacons)) == cfg.inbeacons
+                    inpeaks = [inpeaks;i];
+                end
+            end
+        end
+    end
     
+    %GetOutPeaks
+    outpeaks = [];
+    for i = 1:length(alloutpeaks)
+        if alloutpeaks(i) == cfg.outbeacons(length(cfg.outbeacons))
+            pastpeaks = alloutpeaks(1:i);
+            pastpeaks(isnan(pastpeaks)) = [];
+            if length(pastpeaks) - length(cfg.outbeacons) >= 0
+                if pastpeaks(length(pastpeaks)-(length(cfg.outbeacons)-1):length(pastpeaks)) == cfg.outbeacons
+                    outpeaks = [outpeaks;i];
+                end
+            end
+        end
+    end
     
-    %GetOutPeak    
+    %Filter In Peaks
+    finalinpeaks = [];
+    for i = 1:length(inpeaks)
+        remove = false;
+        
+        if (max(outpeaks)<inpeaks(i))
+            remove = true;
+        end
+        
+        comingpeaks = allpeaks(i:length(allpeaks));
+        if ~any(comingpeaks(:) == cfg.middlebeacon)
+            remove = true;
+        end
+        
+        if remove == false
+            %inpeaks(i) = [];
+            finalinpeaks = [finalinpeaks;inpeaks(i)];
+        end
+    end
+    inpeaks = finalinpeaks;
+    
+    %Filter Out Peaks
+    finaloutpeaks = [];
+    for i = 1:length(outpeaks)
+        remove = false;
+        
+        if (min(inpeaks)>outpeaks(i))
+            remove = true;
+        end
+        
+        pastpeaks = allpeaks(1:outpeaks(i));
+        if ~any(pastpeaks(:) == cfg.middlebeacon)
+            remove = true;
+        end
+        
+        if remove == false
+            finaloutpeaks = [finaloutpeaks;outpeaks(i)];
+        end
+    end
+    outpeaks = finaloutpeaks;
     
     %% In Out Visualization
     if (cfg.checkdata == true)
@@ -192,28 +274,44 @@ while (repeatdetection == 'y')
         
         hold on
         
+        dataMin = min(cell2mat(struct2cell(beaconsData)));
+        dataMax = max(cell2mat(struct2cell(beaconsData)));
+
         xmin = 0;
         xmax = length(beaconData.time);
-        yrange = max(indata)-min(indata);
-        ymin = clamp(min(indata) - (yrange/10),0,min(indata));
-        ymax = max(indata) + (yrange/10);
+        yrange = dataMax-dataMin;
+        ymin = clamp(dataMin - (yrange/10),0,dataMin);
+        ymax = dataMax + (yrange/10);
         
-        xlim([xmin xmax])
-        ylim([ymin ymax])
+        xlim([xmin xmax]);
+        ylim([ymin ymax]);
         
-        rectangle('Position',[xmin ymin xmax-xmin cfg.minstrength-ymin], 'FaceColor', '#d4ffe0');
+        rectangle('Position',[xmin ymin xmax-xmin cfg.minstrength-ymin], 'FaceColor', '#d4ffe0');        
         
-        if (inbeaconIndex == outbeaconIndex)
-            plot(beaconData.time,indata,'-k')
-        else
-            plot(beaconData.time,indata,'-b')
-            plot (beaconData.time,outdata,'-r')
+        %Visualize beacons in figure
+        names = fieldnames(beaconsData);
+        for i = 1:length(names)
+            plot(beaconData.time,beaconsData.(names{i}));
         end
-        xline(inpeak,'--b')
-        xline(outpeak,'--r')
+        for i = 1:length(allpeaks)
+            if allpeaks(i) == cfg.middlebeacon
+                xline(i,'--g');
+            end
+        end
         
-        text(inpeak+1, max(indata), 'In', 'Color', 'blue');
-        text(outpeak+1, max(indata), 'Out', 'Color', 'red');
+        %Visualize In / Out Text
+        t=[];
+        for i = 1:length(inpeaks)
+            t(length(t)+1) = text(inpeaks(i), dataMax, "In", 'Color', 'blue');
+            xline(inpeaks(i),'--b');
+        end
+        for i = 1:length(outpeaks)
+            t(length(t)+1) = text(outpeaks(i), dataMax, "Out ", 'Color', 'red');
+            xline(outpeaks(i),'--r');
+        end
+        
+        
+        %t(2) = text(outpeak+1, max(indata), 'Out', 'Color', 'red');
         
         hold off
         
@@ -239,19 +337,116 @@ while (repeatdetection == 'y')
                 repeatset = 1;
                 
             else
-                disp("Invalid Input, ");
+                disp("Invalid Input");
                 
                 repeatset = 0;
             end
         end
-        
-        if ishandle(fig)
-            close(fig)
-        end
-        
     else
         repeatdetection = 'n';
     end
+end
+
+%% Check and Ask Multiple In / Out
+%Get rid of old text on figure
+for i = 1:length(t)
+    delete(t(i))
+end
+t = [];
+
+%Check In Peaks
+if length(inpeaks) > 1
+    for i = 1:length(inpeaks)
+        t(length(t)+1) = text(inpeaks(i), dataMax, strcat("In ",string(i)), 'Color', 'blue');
+    end
+    
+    repeatset = 0;
+    while repeatset == 0
+        
+        prompt = 'Click on the plot to select the IN that you want to use?';
+        warning(prompt);
+        [x,y] = ginput(1);
+        [~,pickedpeak] = (min(abs(inpeaks - x)));
+        
+        li = xline(inpeaks(pickedpeak),'--b', 'LineWidth', 2);
+        
+        prompt = 'Is this the correct IN peak? y/n [n]: ';
+        peakcorrect = strtrim(input(prompt,'s'));
+        if isempty(peakcorrect)
+            peakcorrect = 'n';
+        end
+        
+        if peakcorrect == 'y'
+            disp('Using Indicated Peak');
+            repeatset = 1;
+            inpeak = inpeaks(pickedpeak);
+        elseif peakcorrect == 'n'
+            disp("Let's try this again");
+            delete(li)
+            repeatset = 0;
+        else
+            disp("Let's try this again");
+            delete(li)
+            repeatset = 0;
+        end
+    end
+elseif isempty(inpeaks)
+    error('No in peak found, stopping this run to mitigate future issues.');
+else
+    inpeak = inpeaks(1);
+end
+
+for i = 1:length(t)
+    delete(t(i))
+end
+t = [];
+
+%Check Out Peaks
+if length(outpeaks) > 1
+    for i = 1:length(outpeaks)
+        t(length(t)+1) = text(outpeaks(i), dataMax, strcat("Out ",string(i)), 'Color', 'red');
+    end
+    
+    repeatset = 0;
+    while repeatset == 0
+        
+        prompt = 'Click on the plot to select the OUT that you want to use?';
+        warning(prompt);
+        [x,y] = ginput(1);
+        [~,pickedpeak] = (min(abs(outpeaks - x)));
+        
+        lo = xline(outpeaks(pickedpeak),'--r', 'LineWidth', 2);
+        
+        prompt = 'Is this the correct OUT peak? y/n [n]: ';
+        peakcorrect = strtrim(input(prompt,'s'));
+        if isempty(peakcorrect)
+            peakcorrect = 'n';
+        end
+        
+        if peakcorrect == 'y'
+            disp('Using Indicated Peak');
+            repeatset = 1;
+            outpeak = outpeaks(pickedpeak);
+        elseif peakcorrect == 'n'
+            disp("Let's try this again");
+            delete(lo)
+            repeatset = 0;
+        else
+            disp("Let's try this again");
+            delete(lo)
+            repeatset = 0;
+        end
+    end
+elseif isempty(inpeaks)
+    error('No in peak found, stopping this run to mitigate future issues.');
+else
+    outpeak = outpeaks(1);
+end
+
+
+%Close the final figure and return the values
+if ishandle(fig)
+    close(fig)
 end
 
 %% Set Out Values based on Inpeak and Outpeak
