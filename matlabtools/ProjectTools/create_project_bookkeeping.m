66a2bf69fe75165f588cffba5d2c434fa7351cca
HRevers <144128560+HRevers@users.noreply.github.com>
2024-05-21 19:25:46 +0200

    Update create_project_bookkeeping.m
    new file structure for saving the data: all processed data is now stored in the same file (per pp), so we need only one folder.
    Changed the 'conditions' to 'segments' to generalize how it can be used.

--- matlabtools/ProjectTools/create_project_bookkeeping.m
+++ matlabtools/ProjectTools/create_project_bookkeeping.m
@@ -1,4 +1,4 @@
-function project = create_project_bookkeeping(project)
+function project = create_project_bookkeeping(cfg, project)
 %% CREATE_PROJECT_BOOKKEEPING
 %  function create_project_bookkeeping(project)
 % 
@@ -7,11 +7,8 @@ function project = create_project_bookkeeping(project)
 %   project.project_name
 %   project.project_directory
 %   project.rawdata_directory
-%   project.segmented_data_directory
-%   project.artifact_corrected_data_directory
-%   project.deconvolved_data_directory
+%   project.processed_data_directory
 %   project.output_directory
-%   project.ask_create_directory
 %
 % The file will be called <project_name>.belt
 % and will be saved at <project_directory>
@@ -24,44 +21,35 @@ end
 if ~isfield(project, 'project_directory')
     error('The project struct has no project_directory field. I cannot create a project bookkeeping file without it.');
 end
-if ~isfield(project, 'nof_ws_conditions')
-    project.nof_ws_conditions = 1;
-    project.condition_names(1) = "Condition1";
+if ~isfield(project, 'nof_segments')
+    project.nof_segments = 1;
+    project.segment(1).name = "Segment1";
 end
 
 
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FOR TESTING, REMOVE WHEN DONE
-project.nof_ws_conditions = 2;
-project.condition_names(2) = "Condition2";
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END FOR TESTING
 
 %% Create the bookkeeping struct
 
-% read the participant labels from the raw data directory
-dinfo = dir(project.raw_data_directory);
-dinfo(ismember({dinfo.name}, {'.', '..'})) = []; % Skip the system entries "." and ".."
-folder_names = {dinfo([dinfo.isdir]).name}'; % get the names of folders only
+% === One possibility to get the participant labels is to extract them 
+%     from the raw data directory. However, that can be a bit of a mess with 
+%     other folders. So perhaps not the best approach ===
+    % dinfo = dir(project.raw_data_directory);
+    % dinfo(ismember({dinfo.name}, {'.', '..'})) = []; % Skip the system entries "." and ".."
+    % folder_names = {dinfo([dinfo.isdir]).name}'; % get the names of folders only
+    % participant_labels = folder_names;
 
-% Perhaps we should do some more checks here, like whether there is an
-% actual datafile in each folder, but for now we will asume each folder
-% holds data of a participant
-participant_labels = folder_names;
-
-% add the bookkeeping struct to the project
-for participant_i=1:length(participant_labels)
-    project.bookkeeping(participant_i).participant_labels = participant_labels(participant_i); % participant labels
-    for condition_i = 1:project.nof_ws_conditions
-        project.bookkeeping(participant_i).condition(condition_i).starttime = 0;
-        project.bookkeeping(participant_i).condition(condition_i).endtime = 0;
-        project.bookkeeping(participant_i).condition(condition_i).segmented = 0;
-        project.bookkeeping(participant_i).condition(condition_i).artifact_corrected = 0;
-        project.bookkeeping(participant_i).condition(condition_i).deconvolved = 0;
-        project.bookkeeping(participant_i).condition(condition_i).include = 1;
-    end
+% add the bookkeeping part to the project
+for segment_i = 1:project.nof_segments
+    % project.segment(segment_i).starttime        = zeros(size(project.pp_labels));; % already extracted from the ParticipantData file
+    % project.segment(segment_i).endtime          = zeros(size(project.pp_labels));; % already extracted from the ParticipantData file
+    project.segment(segment_i).segmented          = zeros(size(project.pp_labels));
+    project.segment(segment_i).artifact_corrected = zeros(size(project.pp_labels));
+    project.segment(segment_i).deconvolved        = zeros(size(project.pp_labels));
+    project.segment(segment_i).include            = ones(size(project.pp_labels));
 end
 
 % note how many variables are stored for each condition
-nof_vars_per_condition =  length(fieldnames(project.bookkeeping(1).condition(1, 1)));
+nof_vars_per_segment =  length(fieldnames(project.segment(1)));
 
 %% Create a text file
 %  the tab (\t) is a separator, it moves the cursor to the next cell
@@ -72,10 +60,13 @@ file_dir = fullfile(project.project_directory, [project.project_name '.belt']);
 [fileID, errmsg] = fopen(file_dir,'w');
 
 % check for errors related to opening the file
-if length(errmsg)
+if ~isempty(errmsg)
     error(errmsg);
 end
 
+% === TODO: check whether project file already existed
+%           if so, compare the project struct to the existing project file
+%           and report ===
 
 % Write some information about this bookkeeping file on the first line
 fprintf(fileID,'This is a BELT project file. Settings and status of a project are stored here. This file is maintained automatically by the BELT app.\n');
@@ -91,24 +82,18 @@ fprintf(fileID,'%s\n',project.project_directory);
 fprintf(fileID,'Raw_data_directory\t');
 fprintf(fileID,'%s\n',project.raw_data_directory); 
 
-fprintf(fileID,'Segmented_data_directory\t');
-fprintf(fileID,'%s\n',project.segmented_data_directory); 
-
-fprintf(fileID,'Artifact_corrected_data_directory\t');
-fprintf(fileID,'%s\n',project.artifact_corrected_data_directory); 
-
-fprintf(fileID,'Deconvolved_data_directory\t');
-fprintf(fileID,'%s\n',project.deconvolved_data_directory); 
+fprintf(fileID,'Processed_data_directory\t');
+fprintf(fileID,'%s\n',project.processed_data_directory); 
 
 fprintf(fileID,'Output_directory\t');
 fprintf(fileID,'%s\n',project.output_directory); 
 
-fprintf(fileID,'Number_of_within_subjects_conditions\t');
-fprintf(fileID,'%i\n',project.nof_ws_conditions); 
+fprintf(fileID,'Number_of_segments\t');
+fprintf(fileID,'%i\n',project.nof_segments); 
 
-fprintf(fileID,'Condition_names\t');
-for condition_i = 1:project.nof_ws_conditions
-    fprintf(fileID,'%s\t',project.condition_names(condition_i));
+fprintf(fileID,'Segment_names\t');
+for segment_i = 1:project.nof_segments
+    fprintf(fileID,'%s\t',project.segment(segment_i).name);
 end
 fprintf(fileID,'\n');
 
@@ -119,10 +104,10 @@ fprintf(fileID,'\n'); % empty row to visually divide this from previous part
 % Add a header to this part
 fprintf(fileID,'Data bookkeeping\t');
 % also show the condition names
-for condition_i = 1:project.nof_ws_conditions
-    fprintf(fileID,'%s\t',project.condition_names(condition_i));
+for segment_i = 1:project.nof_segments
+    fprintf(fileID,'%s\t',project.segment(segment_i).name);
     % skip some cells to fit all variables per condition
-    for variable_i = 1:(nof_vars_per_condition-1) % create an empty cell for each variable that we keep track of minus 1, before entering the next condition header
+    for variable_i = 1:(nof_vars_per_segment-1) % create an empty cell for each variable that we keep track of minus 1, before entering the next condition header
         fprintf(fileID,'\t'); % empty cell
     end
 end
@@ -130,7 +115,7 @@ fprintf(fileID,'\n');
 
 % add the table headers
 fprintf(fileID,'Participants\t');
-for condition_i = 1:project.nof_ws_conditions
+for segment_i = 1:project.nof_segments
     fprintf(fileID,'StartTime\t');
     fprintf(fileID,'EndTime\t');
     fprintf(fileID,'Segmented\t');
@@ -142,20 +127,20 @@ fprintf(fileID,'\n');
 
 
 % create a row for each participant
-for participant_i=1:length(participant_labels)
-    fprintf(fileID,'%s\t',string(project.bookkeeping(participant_i).participant_labels));
-    for condition_i = 1:project.nof_ws_conditions
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).starttime));
+for pp_i=1:length(project.pp_labels)
+    fprintf(fileID,'%s\t',string(project.pp_labels(pp_i)));
+    for segment_i = 1:project.nof_segments
+        fprintf(fileID, string(project.segment(segment_i).starttime(pp_i)));
         fprintf(fileID,'\t');
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).endtime));
+        fprintf(fileID, string(project.segment(segment_i).endtime(pp_i)));
         fprintf(fileID,'\t');
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).segmented));
+        fprintf(fileID, string(project.segment(segment_i).segmented(pp_i)));
         fprintf(fileID,'\t');
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).artifact_corrected));
+        fprintf(fileID, string(project.segment(segment_i).artifact_corrected(pp_i)));
         fprintf(fileID,'\t');
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).deconvolved));
+        fprintf(fileID, string(project.segment(segment_i).deconvolved(pp_i)));
         fprintf(fileID,'\t');
-        fprintf(fileID, string(project.bookkeeping(participant_i).condition(condition_i).include));
+        fprintf(fileID, string(project.segment(segment_i).include(pp_i)));
         fprintf(fileID,'\t');
     end
     fprintf(fileID,'\n');
